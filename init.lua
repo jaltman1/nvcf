0efbba53a635570d2d1faa44d74124ada07665b7
Jackson Altman <jwaltman00@gmail.com>
2022-04-15 00:05:13 -0400

    Set up autocompletion and go

--- init.lua
+++ init.lua
@@ -8,14 +8,41 @@ Plug('junegunn/fzf', {
 		vim.call('fzf#install()')
 	end
 })
+
+-- Not sure what this does yet
 Plug('nvim-treesitter/nvim-treesitter', {['do'] = vim.fn['TSUpdate']})
+
+-- Adds this theme
 Plug('folke/tokyonight.nvim', { branch = 'main' })
+
+--Language server support
+Plug('neovim/nvim-lspconfig')
+
+-- Many go features supported including some overlap with gopls lspconfig, should at least share a gopls instance
+Plug('fatih/vim-go', {['do'] = 'GoUpdateBinaries'})
+
+-- Autocompletion
+Plug 'hrsh7th/cmp-nvim-lsp'
+Plug 'hrsh7th/cmp-buffer'
+Plug 'hrsh7th/cmp-path'
+Plug 'hrsh7th/cmp-cmdline'
+Plug 'hrsh7th/nvim-cmp'
+Plug 'hrsh7th/cmp-vsnip'
+Plug 'hrsh7th/vim-vsnip'
 
 vim.call('plug#end')
 
+-- Basic keymaps i use
 require('keymaps')
+
+-- Settings i would set in any vim i use anywhere
 require('basic')
-require'nvim-treesitter.configs'.setup {
+
+-- Set up for all lsp and lsp related stuff like snippets and autocompletion
+require('lsp')
+
+-- Dont know what this stuff does but i think it is helping?
+require('nvim-treesitter.configs').setup {
   highlight = {
     enable = true,
   },
@@ -24,6 +51,7 @@ require'nvim-treesitter.configs'.setup {
   }
 }
 
+--Style configuration
 vim.g.tokyonight_style = "night"
 vim.cmd[[colorscheme tokyonight]]
 
---
+++ lua/lsp.lua
@@ -0,0 +1,92 @@
+
+
+local opts = { noremap=true }
+vim.api.nvim_set_keymap('n', '<space>e', '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
+vim.api.nvim_set_keymap('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
+vim.api.nvim_set_keymap('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
+vim.api.nvim_set_keymap('n', '<space>q', '<cmd>lua vim.diagnostic.setloclist()<CR>', opts)
+
+local generic_on_attach = function(client, bufnr)
+    vim.api.nvim_buf_set_keymap(bufnr, 'n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
+end
+
+
+
+local cmp = require'cmp'
+
+  cmp.setup({
+    snippet = {
+      expand = function(args)
+        vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+      end,
+    },
+    window = {
+      completion = cmp.config.window.bordered(),
+      documentation = cmp.config.window.bordered(),
+    },
+    mapping = {
+    ["<Tab>"] = cmp.mapping(function(fallback)
+      -- This little snippet will confirm with tab, and if no entry is selected, will confirm the first item
+      if cmp.visible() then
+        local entry = cmp.get_selected_entry()
+	if not entry then
+	  cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+	else
+	  cmp.confirm()
+	end
+      else
+        fallback()
+      end
+    end, {"i","s","c",}),
+      --['<C-b>'] = cmp.mapping.scroll_docs(-4),
+      --['<C-f>'] = cmp.mapping.scroll_docs(4),
+      --['<C-Space>'] = cmp.mapping.complete(),
+      --['<C-e>'] = cmp.mapping.abort(),
+      --['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+    },
+    sources = cmp.config.sources({
+      { name = 'nvim_lsp' },
+      { name = 'vsnip' }, -- For vsnip users.
+    }, {
+      { name = 'buffer' },
+    })
+  })
+  cmp.setup.filetype('gitcommit', {
+    sources = cmp.config.sources({
+      { name = 'cmp_git' }, -- You can specify the `cmp_git` source if you were installed it.
+    }, {
+      { name = 'buffer' },
+    })
+  })
+
+  -- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
+  cmp.setup.cmdline('/', {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = {
+      { name = 'buffer' }
+    }
+  })
+
+  -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+  cmp.setup.cmdline(':', {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = cmp.config.sources({
+      { name = 'path' }
+    }, {
+      { name = 'cmdline' }
+    })
+  })
+
+  -- Setup lspconfig.
+  local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+  -- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
+
+local go_on_attach = function(client, bufnr)
+    generic_on_attach(client, bufnr)
+    vim.api.nvim_buf_set_keymap(bufnr, 'n', ',r', ':GoRun %<Enter>', opts)
+end
+require('lspconfig').gopls.setup {
+    capabilities = capabilities,
+    on_attach = go_on_attach,
+    cmd = {'gopls', '-remote=auto'}
+}
